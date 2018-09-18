cd3b6b822c52bdc54d61f33ba991d18bc981fb3b
Alejandro Metke <alejandro.metke@csiro.au>
2018-09-18 13:10:14 +0100

    Initial commit.

---
+++ src/main/java/au/csiro/gelcustomdisordermapping/GelCustomDisorderMappingApplication.java
@@ -0,0 +1,12 @@
+package au.csiro.gelcustomdisordermapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelCustomDisorderMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GelCustomDisorderMappingApplication.class, args);
+	}
+}
---
+++ src/test/java/au/csiro/gelcustomdisordermapping/GelCustomDisorderMappingApplicationTests.java
@@ -0,0 +1,16 @@
+package au.csiro.gelcustomdisordermapping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GelCustomDisorderMappingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
