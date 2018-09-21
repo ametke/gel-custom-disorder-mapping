e06741f94a0796e285d4343345dfd6a027a252dc
Javi Lopez <javild@gmail.com>
2018-09-21 16:55:15 +0100

    disorder to panel mapping refined

--- src/test/python/get_disease2panel_map.py
+++ src/test/python/get_disease2panel_map.py
@@ -1,8 +1,27 @@
+import os
+
 import requests
+
+
+def _load_recruited_disorders():
+    disorder_set = set([])
+    resources_directory = os.path.join(os.path.dirname(os.getcwd()))
+    fd = open(os.path.join(resources_directory, "rare_diseases.csv"))
+    fd.readline()
+    for line in fd:
+        parts = line.split(",")
+        disorder_set.add(parts[1])
+        disorder_set.add(parts[3])
+        disorder_set.add(parts[5])
+        disorder_set.add(parts[7])
+    fd.close()
+
+    return disorder_set
 
 
 if __name__ == '__main__':
 
+    recruited_disorders = _load_recruited_disorders()
     more = True
     count = 0
     disease2panel = {}
@@ -13,10 +32,16 @@ if __name__ == '__main__':
         i += 1
         for p in panels_page['results']:
             panel_code = "{id}-{version}".format(id=p["id"], version=p["version"])
-            for disorder in p["relevant_disorders"]:
-                panels_list = disease2panel.get(disorder, [])
+            if p["name"] in recruited_disorders:
+                panels_list = disease2panel.get(p["id"], [])
                 panels_list.append(panel_code)
-                disease2panel[disorder] = panels_list
+                disease2panel[p["name"]] = panels_list
+            elif p["relevant_disorders"]:
+                for disorder in p["relevant_disorders"]:
+                    if disorder in recruited_disorders:
+                        panels_list = disease2panel.get(disorder, [])
+                        panels_list.append(panel_code)
+                        disease2panel[disorder] = panels_list
             count += 1
         if count >= total_count:
             more = False
